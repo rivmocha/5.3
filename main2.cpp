5b0fc3b11c5e2923b61cb07d239dfd5d26af0456
mocha <skykww0714@gmail.com>
2022-05-03 15:24:38 +0900

    t. shuffle

--- main2.cpp
+++ main2.cpp
@@ -1,40 +1,91 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
-#define Num 10
+
+#define MAX 52
+
+//Heart A           0 - 12    0
+//Spade A           13 - 25   1
+//Clover A          26 - 38   2
+//Diamond A         39 - 51   3
+
 
 
 int main()
 {
-    int A[Num] = { 0, };
-    
+    int A[MAX] = { 0, };
+    int SplitCard[52];
+
+
     srand((unsigned int)time(0));
 
-    for (int i = 0; i < Num; ++i)
+    //Initialize
+    for (int i = 0; i < MAX; ++i)
     {
-        A[i] = i + 1;
+        A[i] = i;
     }
 
     //shuffle
-    for (int i = 0; i < Num * 2; ++i)
+    for (int i = 0; i < MAX * 2; ++i)
     {
-        int FirstNumber = rand() % Num;
-        int SecondNumber = rand() % Num;
+        int FirstNumber = rand() % MAX;
+        int SecondNumber = rand() % MAX;
 
         //Swap
         int Temp = A[FirstNumber];
         A[FirstNumber] = A[SecondNumber];
         A[SecondNumber] = Temp;
     }
-    for (int i = 0; i < Num; ++i)
+
+    //split
+    for (int i = 0; i < 2; ++i)
     {
-        cout << A[i] << endl;
+        SplitCard[i] = A[i];
     }
+
+    for (int i = 0; i < 2; ++i)
+    {
+        int Type = SplitCard[i] / 13;
+        int Number = SplitCard[i] % 13;
+
+        switch (Type)
+        {
+        case 0:
+            cout << "H";
+            break;
+        case 1:
+            cout << "S";
+            break;
+        case 2:
+            cout << "C";
+            break;
+        case 3:
+            cout << "D";
+            break;
+        }
+
+        switch (Number)
+        {
+        case 12:
+            cout << "K" << " ";
+            break;
+        case 11:
+            cout << "Q" << " ";
+            break;
+        case 10:
+            cout << "J" << " ";
+            break;
+        case 0:
+            cout << "A" << " ";
+            break;
+        default:
+            cout << Number + 1 << " ";
+            break;
+        }
+    }
+
+
+
 
     return 0;
 }
-
-
-
-
-
