c5e6cdb3b6220a54591ed43c08b887bacc21ad9f
mocha <skykww0714@gmail.com>
2022-05-09 13:02:21 +0900

    블랙잭

--- main2.cpp
+++ main2.cpp
@@ -5,6 +5,9 @@ using namespace std;
 int A[MAX] = { 0, };
 int SplitCard[52];
 
+
+
+
 int main()
 {
     srand((unsigned int)time(0));
@@ -20,8 +23,8 @@ int main()
 
     for (int i = 0; i < MAX * 2; ++i)
     {
-        int FirstNumber = rand() % MAX;
-        int SecondNumber = rand() % MAX;
+        int FirstNumber = rand() % MAX;  
+        int SecondNumber = rand() % MAX; 
 
         //Swap
         int Temp = A[FirstNumber];
@@ -30,13 +33,13 @@ int main()
     }
  
 
-
     //split
     {
         for (int i = 0; i < 2; ++i)
         {
             SplitCard[i] = A[i];
         }
+
 
         for (int i = 0; i < 2; ++i)
         {
@@ -46,16 +49,16 @@ int main()
             switch (Type)
             {
             case 0:
-                cout << "H";
+                cout << "Heart";
                 break;
             case 1:
-                cout << "S";
+                cout << "Spade";
                 break;
             case 2:
-                cout << "C";
+                cout << "Club";
                 break;
             case 3:
-                cout << "D";
+                cout << "Diamond";
                 break;
             }
 
@@ -79,6 +82,61 @@ int main()
             }
         }
     }
+
+
+    //Draw
+    int Playercard[3] = { 0, };
+    int Dealercard[3] = { 0, };
+    
+    for (int i = 0; i < 3; ++i)
+    {
+        Playercard[i] = A[i * 2];
+        Dealercard[i] = A[(i * 2) + 1];
+    }
+
+
+
+    // reslut
+
+    // 
+    int PCardsum = 0;
+    int DCardsum = 0;
+        
+    for (int i = 0; i < 3; ++i)
+    {
+        PCardsum += Playercard[i] > 10 ? 10 : Playercard[i];
+        DCardsum += (Dealercard[i] > 10) ? 10 : Dealercard[i];
+    }
+
+    cout << "Playercard : " << PCardsum << endl;
+    cout << "Dealercard : " << DCardsum << endl;
+
+    // 플레이어 승리 조건
+    // 딜러카드 합이 21을 초과할경우
+    // 둘다 21 이하일때는 카드합이 플레이어가 같거나 높을경우
+    if (DCardsum > 21)
+    {
+        cout << "Player Win" << endl;
+    }
+    else if (DCardsum <= 21 && PCardsum <= 21)
+    {
+        if (PCardsum >= DCardsum)
+        {
+            cout << "Player Win" << endl;
+        }
+        else
+        {
+            cout << "AI Win" << endl;
+        }
+    }
+    else
+    {
+        cout << "AI Win" << endl;
+    }
+    
+
+
+
 
     return 0;
 }
